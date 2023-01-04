489844b1880bbd80b264e30991c4374272f6450b
Zeshawn Manzoor <zeshawnmanzoor@yahoo.com>
2023-01-04 12:09:50 -0500

    Update 1-4-23

--- app.py
+++ app.py
@@ -9,12 +9,13 @@
 
 # print("I'M STILL HERE DOG!")
 
+name = input("Name: ")
 adjective = input("Adjective: ")
 good = input("Good: ")
 bad = input("Bad: ")
 reason = input("Reason: ")
 goal = input("Goal: ")
 
-print(f"Programming can be very {adjective} sometimes. The best part about it is the {good}.\
+print(f"My name is {name}. Programming can be very {adjective} sometimes. The best part about it is the {good}.\
 The worst part about it is the {bad}. I know I will be able to acheive this and become a software engineer because {reason}. \
 In 5 years from now, I hope to {goal}.")
